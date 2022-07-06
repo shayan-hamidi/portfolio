854dce083d6618eb117969e45d12736e29a3be62
s.hamidi <sha.hamidi10@gmail.com>
2022-07-06 20:16:33 +0430

    test

--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -4,6 +4,7 @@ import { MdOutlineEmail } from "react-icons/md";
 import { AiFillFacebook } from "react-icons/ai";
 import { BsWhatsapp } from "react-icons/bs";
 import emailjs from "emailjs-com";
+
 function Contact() {
   const form = useRef();
   const constsendEmail = (e) => {
